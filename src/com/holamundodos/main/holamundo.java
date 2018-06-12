d11dd5e682f535ae5729155d9e60e0281cd36c9d
g2Monik <g2monik@gmail.com>
2018-06-12 12:29:55 -0500

    cuarta version del proyeto

--- src/com/holamundodos/main/holamundo.java
+++ src/com/holamundodos/main/holamundo.java
@@ -7,6 +7,7 @@ public class holamundo {
 		System.out.println("Hola Mundo");
 		System.out.println("Hola Mundo 2");
 		System.out.println("Hola Mundo 3");
+		System.out.println("Hola Mundo 4");
 	}
 
 }
