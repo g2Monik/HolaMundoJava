676ce07768da01f30aed7d72ff704550a0fe74d8
g2Monik <g2monik@gmail.com>
2018-06-12 12:26:48 -0500

    Primera version del proyeto

---
+++ src/com/holamundodos/main/holamundo.java
@@ -0,0 +1,11 @@
+package com.holamundodos.main;
+
+public class holamundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo2");
+	}
+
+}
